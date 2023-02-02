a096f8e31c28965564beac9e02170bd5de3d0083
Victor Lopez <victorlpz2301@gmail.com>
2023-02-02 09:56:34 +0100

    iteration1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+    title: {
+      type: String,
+      required: "title is required"
+    },
+    level: String,
+    ingredients: [String],
+    cuisine:  {
+      type: String,
+      required: "cuisine is required"
+    },
+    dishType: String,
+    image: String,
+    duration: Number,
+    creator: String,
+    created: Date,
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
